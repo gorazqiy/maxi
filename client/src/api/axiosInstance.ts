5a6ad6831146460df2568d2b138d11e6104c6989
Elisey <gorazqiy@gmail.com>
2026-06-16 07:34:52 +0300

    w

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -26,8 +26,11 @@ axiosInstance.interceptors.response.use(
    (response) => response,
    (error) => {
       if (error.response?.status === 401) {
-         localStorage.removeItem("token");
-         window.location.href = "/login";
+         const token = localStorage.getItem("token");
+         if (token) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+         }
       }
       return Promise.reject(error);
    },
